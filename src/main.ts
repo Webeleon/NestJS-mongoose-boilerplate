49ea28d04c5f391116d5ab3362ba5852f0d2b443
Julien Prugne <julien@webeleon.dev>
2022-08-15 11:33:42 +0200

    chore: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConfig } from './config/app.config';
+import { ConfigType } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as pkg from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('rsvp api')
+    .setVersion(pkg.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
+  const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+  await app.listen(config.port);
+  Logger.log(`API started on port ${config.port}`, 'MAIN');
 }
 bootstrap();
