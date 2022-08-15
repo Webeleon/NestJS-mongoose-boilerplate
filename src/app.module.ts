1ca726b69c68ea73f19ff84da2ee601d3105af15
Julien Prugne <julien@webeleon.dev>
2022-08-15 11:20:55 +0200

    chore: configure mongodb database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType, getConfigToken } from '@nestjs/config';
+import { appConfig } from './config/app.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [databaseConfig.KEY],
+      useFactory: async (config: ConfigType<typeof databaseConfig>) => ({
+        uri: config.mongoUrl,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,8 @@
+import { registerAs } from '@nestjs/config';
+
+export const databaseConfig = registerAs('database', () => ({
+  mongoUrl:
+    process.env.MONGO_URL ||
+    process.env.MONGODB_ADDON_URI ||
+    'mongodb://localhost/audrey-et-clem',
+}));
