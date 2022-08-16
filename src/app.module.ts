88feba7b24ca271784bad730a56d392f8c30e4f4
Julien Prugne <julien@webeleon.dev>
2022-08-16 08:51:57 +0200

    feat: add health check on /
    - new endpoint on /
    - add @shelf/jest-mongodb for testing with a mongo in memory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigType, getConfigToken } from '@nestjs/config';
 import { appConfig } from './config/app.config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { databaseConfig } from './config/database.config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -14,14 +15,18 @@ import { ThrottlerModule } from '@nestjs/throttler';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [databaseConfig.KEY],
-      useFactory: async (config: ConfigType<typeof databaseConfig>) => ({
-        uri: config.mongoUrl,
-      }),
+      useFactory: async (config: ConfigType<typeof databaseConfig>) => {
+        Logger.debug(config, 'database config');
+        return {
+          uri: config.mongoUrl,
+        };
+      },
     }),
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 10,
     }),
+    HealthModule,
   ],
 })
 export class AppModule {}
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,8 +2,12 @@ import { registerAs } from '@nestjs/config';
 
 export const databaseConfig = registerAs('database', () => ({
   mongoUrl:
-    process.env.MONGO_URL ||
+    // in memory mongo for tests
+    global.__MONGO_URI__ ||
+    // .env override
+    process.env.MONGO_URI ||
     // clevercloud env variable
     process.env.MONGODB_ADDON_URI ||
+    // default value
     'mongodb://localhost/nestjs-boilerplate',
 }));
