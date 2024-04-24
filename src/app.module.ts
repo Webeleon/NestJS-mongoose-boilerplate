9d981114dbe9a97c984cc562f1b94b5bb59f8f2f
Julien Prugne <julien@webeleon.dev>
2024-04-24 10:19:57 +0200

    chore: update and fix dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule, ConfigType, getConfigToken } from '@nestjs/config';
 import { appConfig } from './config/app.config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { databaseConfig } from './config/database.config';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { HealthModule } from './health/health.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -21,11 +22,20 @@ import { HealthModule } from './health/health.module';
         };
       },
     }),
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 10,
-    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 50,
+        name: 'standard',
+      },
+    ]),
     HealthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
